0c7a00cc5cd4154d3debe205e75cf8863fa2bd2d
choijiwoong <gogogi313@naver.com>
2021-11-27 09:09:35 +0900

    주석달기완료! 이해는 약간씩 되어가는 듯 하지만 역시 어렵다!! 지금 보고있는게 총 5강인 만큼 한강한강 이해안되어서 넘어가는 부분 없도록 잘 보쟈

--- Window API 소개/3. Test code.c
+++ Window API 소개/3. Test code.c
@@ -1,3 +1,24 @@
+/*
+1. RegWindowClass(Set Window class property, Register)
+2. Make Window Instance, Virtualize, Call MessageLoop for handling message
+3. In MessageLoop, handling message like WM_QUIT, WM_CHAR with dispatch for using it in procedure 
+4. In Procedure, handle message by WM_PAINT to OnPaint function call, WM_DESTROY to OnDestroy function call.
+   returning DefWindowProcedure for print on screen.
+5. In OnPaint, Make PaintStruct, call BeginPaint function for calculating unvalid section & registering DC.
+   And call OnDraw function for drawing figure actually.
+6. In OnDraw, Make Pen&Brush object and Pass to DrawDiagram function with set RECT size
+7. In DrawDiagram, Draw Rectangle & Ellipse
+
+8. In DrawDiagram, Get initial handle of HPEN, HBRUSH
+9. In OnDraw, Delete all of Pen & Brush Objects
+10.In OnPaint, Call EndPaint function changing to valid section & deleting DC
+11.In Procedure, if it got WM_DESTROY message(Click close instance) and call OnDestroy function
+12.In OnDestroy, PostQuitMessage(0) for escape MessageLoop
+13.In MessageLoop, break for loop
+14.In Procedure return DefWindowProcedure for print on screen.
+15.In WinMain, MessageLoop is done. so return 0 for exit this program.
+*/
+
 #include <Windows.h>
 #define MY_DRAW_WND (TEXT("ex_drawing"))//for class name
 
@@ -26,12 +47,13 @@ INT APIENTRY WinMain(HINSTANCE hIns, HINSTANCE hPrev, LPSTR cmd, INT nShow)
 LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);//definition
 void RegWindowClass()//Window class property set&register
 {
+	//Set window class property
 	WNDCLASS wndclass={0};
 	wndclass.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH);//Handle of white brush
 	wndclass.hCursor=LoadCursor(0, IDC_ARROW);//Handle of mouse cursor
 	wndclass.hIcon=LoadIcon(0, IDI_APPLICATION);//Handle of icon
 	wndclass.hInstance=GetModuleHandle(0);//Handle of instance in self module
-	wndclass.lpfnWndProc=MyWndProc;//already defined! but not created
+	wndclass.lpfnWndProc=MyWndProc;//already defined! but not created_SET CALLBACK PROC
 	wndclass.lpszClassName=MY_DRAW_WND;//Class Name
 	wndclass.style=CS_DBLCLKS;//Class kind
 	
@@ -52,10 +74,10 @@ void OnPaint(HWND hWnd);
 void OnDestroy(HWND hWnd);
 LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)//creation
 {
-	switch(iMessage)
+	switch(iMessage)//By message, call handler of message
 	{
 		case WM_PAINT:
-			OnPaint(hWnd);
+			OnPaint(hWnd);//call OnPaint
 			return 0;
 		case WM_DESTROY:
 			OnDestroy(hWnd);
@@ -70,9 +92,9 @@ LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPara
 #define RIGHT(pi) ((pi+1)*STEP_WIDTH-20)//upper y per brush index
 #define TOP(bi) (bi*STEP_HEIGHT+30)//right x per pen index
 #define BOTTOM(bi) ((bi+1)*STEP_HEIGHT-30)//bottom y per brush index
-enum EN_PEN {HP_RED, HP_GREEN, HP_BLUE, HP2_RED, HPD_RED, HP_MAX};//Pen style enum
+enum EN_PEN {HP_RED, HP_GREEN, HP_BLUE, HP2_RED, HPD_RED, HP_MAX};//Pen style enum with MAX_value.
 enum EH_BRUSH {HB_RED, HB_GREEN, HB_BLUE, HBS_RED, HB_MAX};//Brush style enum
-
+//Const String *
 LPCTSTR pstrs[HP_MAX]={TEXT("R"), TEXT("G"), TEXT("B"), TEXT("T R"), TEXT("DD R")};//StringArray of pen print
 LPCTSTR bstrs[HB_MAX]={TEXT("R"), TEXT("G"), TEXT("B"), TEXT("C R")};//StringArray of brush print
 
@@ -96,22 +118,22 @@ void OnDraw(HWND hWnd, HDC hdc)
 	hBrushes[HBS_RED]=CreateHatchBrush(HS_CROSS, RGB(255,0,0));
 	
 	TCHAR buf[256]=TEXT("");
-	RECT rt;
+	RECT rt;//rectangle
 	for(int pi=0; pi<HP_MAX; pi++)
 	{
 		for(int bi=0; bi<HB_MAX; bi++)
 		{
 			//set style of Pen & Brush
-			wsprintf(buf, TEXT("%s %s"), pstrs[pi], bstrs[bi]);
+			wsprintf(buf, TEXT("%s %s"), pstrs[pi], bstrs[bi]);//save string to buf
 			//Set Point for print
 			rt.left=LEFT(pi);//mecro
 			rt.top=TOP(bi);
 			rt.right=RIGHT(pi);
 			rt.bottom=BOTTOM(bi);
 			//printString style of Pen & Brush
-			TextOut(hdc, pi*STEP_WIDTH, bi*STEP_HEIGHT, buf, lstrlen(buf));
+			TextOut(hdc, pi*STEP_WIDTH, bi*STEP_HEIGHT, buf, lstrlen(buf));//textprint by buf
 			//print Figure by special Pen & Brush
-			DrawDiagram(hWnd, hdc, hPens[pi], hBrushes[bi], &rt);
+			DrawDiagram(hWnd, hdc, hPens[pi], hBrushes[bi], &rt);//Call DrawDiagram with HPEN, HBRUSH
 		}
 	}
 	
@@ -128,7 +150,7 @@ void OnDraw(HWND hWnd, HDC hdc)
 }
 
 //Draw Figure
-void DrawDiagram(HWND hWnd, HDC hdc, HPEN hPen, HBRUSH hBrush, LPRECT prt)
+void DrawDiagram(HWND hWnd, HDC hdc, HPEN hPen, HBRUSH hBrush, LPRECT prt)//handle, HINSTANCE, ,, rectangle
 {
 	HPEN oPen;//for remember initial handle of Pen
 	oPen=(HPEN)SelectObject(hdc, hPen);//save
@@ -137,18 +159,18 @@ void DrawDiagram(HWND hWnd, HDC hdc, HPEN hPen, HBRUSH hBrush, LPRECT prt)
 	oBrush=(HBRUSH)SelectObject(hdc, hBrush);//save
 	
 	Rectangle(hdc, prt->left, prt->top, prt->right, prt->bottom);//draw rectangle
-	Ellipse(hdc, prt->left, prt->top, prt->right, prt->bottom);//dwar ellipse
+	Ellipse(hdc, prt->left, prt->top, prt->right, prt->bottom);//draw ellipse
 	
-	SelectObject(hdc, oPen);//select initial pen to DC
-	SelectObject(hdc, oBrush);//select initial brush to DC
+	SelectObject(hdc, oPen);//select initial pen to DC. DRAW!
+	SelectObject(hdc, oBrush);//select initial brush to DC. DRAW!
 }
 
 //Handling part of message WM_PAINT
 void OnPaint(HWND hWnd)
 {
-	PAINTSTRUCT ps;
+	PAINTSTRUCT ps;//Make PAINTSTRUCT
 	BeginPaint(hWnd, &ps);//Cal unvalid section & Register DC
-	OnDraw(hWnd, ps.hdc);//Wrok Drawwing
+	OnDraw(hWnd, ps.hdc);//Work Drawwing_CALL ONDRAW!
 	EndPaint(hWnd, &ps);//Change to valud & Delete DC
 }
 
