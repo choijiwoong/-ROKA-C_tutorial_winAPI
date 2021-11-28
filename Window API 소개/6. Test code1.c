8a6f469ce4ab76d1c85541b04eab6fbb01aac1ae
choijiwoong <gogogi313@naver.com>
2021-11-28 22:58:47 +0900

    거의 Timer 완전정복 급으로 완벽하게 이해해서 주석 달아둠. Timer가 WinProc에 발생시키는 Message와 program 실행, 종료 시 발생하는 Message를 하나의 Switch로 처리한거 유의.

--- Window API 소개/6. Test code1.c
+++ Window API 소개/6. Test code1.c
@@ -1,3 +1,15 @@
+/*
+1.	In WinMain, Set properties of window & Register -> Make window object * Visualize -> Call MsgLoop(Execute MyWindowProc)
+2.	In MyWindowProc, WM_CREATE call OnCreate || WM_PAINT call OnPaint || WM_TIMER call OnTimer || WM_DESTROY call OnDestroy.
+	2-1) In OnCreate, Make Timer objects by SetTimer. We set first Timer with OnClock Proc, second Timer without Proc.
+		2-1-1) In OnClock, Get current system time to st(SYSTEMTIME global variable) & invaliate Rect
+	2-2) In OnPaint, Make PAINTSTRUCT object for drawing, call BeginPaint(reg DC, cal unvalid) & OnDraw(drawing) & EndPaint(del DC, change to valid)
+		2-2-1) In OnDraw, TextOut st, ticks that are global variable of information in first, second timer
+	2-3) In OnTimer, If wParam is equals to Serial of second Timer, Call OnTick function.
+		2-3-1) In OnTick, INC ticks & invalidate Rect.
+	2-4) In OnDestroy, delete both Timer by calling KillTimer function & Send WM_QUIT message to window Proc.
+*/
+
 #include <Windows.h>
 #define MY_DEF_STYLE CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS
 
@@ -13,16 +25,16 @@ INT APIENTRY WinMain(HINSTANCE hIns, HINSTANCE hPrev, LPSTR cmd, INT nShow)
 	MsgLoop();
 }
 
-void RegMyClass(LPCWSTR cname)//Set window property, Register
+void RegMyWndClass(LPCWSTR cname)//Set window property, Register
 {
 	WNDCLASS wndclass={0};//Set window's properties
 	wndclass.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH);
 	wndclass.hInstance=GetModuleHandle(0);
 	wndclass.hIcon=LoadIcon(0, IDI_APPLICATION);
 	wndclass.hCursor=LoadCursor(0, IDC_ARROW);
-	wndclass.lpfnWndProc=MyWndProc;
+	wndclass.lpfnWndProc=MyWndProc;//Changing value, call callbackProc
 	wndclass.lpszMenuName=0;
-	wndclass.lpszClassName=0;
+	wndclass.lpszClassName=cname;
 	wndclass.style=MY_DEF_STYLE;
 	
 	RegisterClass(&wndclass);//Register.
@@ -39,7 +51,7 @@ void MakeWindow(LPCWSTR cname)//make window object, Show
 					GetModuleHandle(0),//handle of module
 					0);//arguemnt to WM_CREATE
 	
-	ShowWinodw(hWnd, SW_SHOW);//Visualization of Window Object.
+	ShowWindow(hWnd, SW_SHOW);//Visualization of Window Object.
 }
 
 void MsgLoop()
@@ -52,23 +64,48 @@ void MsgLoop()
 }
 
 
-SYSTEMTIME st;
-int ticks;
-VOID CALLBACK OnClock(HWND hWnd, UINT iMessage, UINT_PTR tid, DWORD cnt)
+SYSTEMTIME st;//Make system time object
+int ticks;//make ticks variable
+VOID CALLBACK OnClock(HWND hWnd, UINT iMessage, UINT_PTR tid, DWORD cnt)//CALLBACK for first Timer
 {
-	GetLocalTime(&st);
+	GetLocalTime(&st);//get local time to st(SYSTEMTIME variable)
+	InvalidateRect(hWnd, 0, 0);//Invalidate Rect. Reflect on Screen per this command.
+	//Make Invalidate Section for redrawing. for all screen redrawing, pass 0 as argument.
+}
+void OnTick(HWND hWnd)//add ticks for second Timer
+{
+	ticks++;//INC ticks
+	InvalidateRect(hWnd, 0, 0);//Invalidate Rect. Reflect on Screen per this command.
+}
+void OnDraw(HDC hdc)//print Timers
+{
+	WCHAR buf[256];
+	wsprintf(buf, TEXT("Current Time: %d:%d:%d"), st.wHour, st.wMinute, st.wSecond);//print SYSTEMTIME
+	TextOut(hdc, 10, 10, buf, lstrlen(buf));
+	wsprintf(buf, TEXT("%8d"), ticks);//print ticks
+	TextOut(hdc, 10, 40, buf, lstrlen(buf));
+}
+void OnPaint(HWND hWnd)
+{
+	PAINTSTRUCT ps;//Make PAINTSTRUCT object for drawing
+	BeginPaint(hWnd, &ps);//calc unvalid section, register dc.
+	OnDraw(ps.hdc);//drawing
+	EndPaint(hWnd, &ps);//change to valid section, delete dc.
 }
 
-#define IDT_CLOCK 1
-#define IDT_TICK 2
+#define IDT_CLOCK 1 //serial id for first Timer(uIDEvent)
+#define IDT_TICK 2 //serial id for second Timer(uIDEvent)
 
-VOID CALLBACK OnClock(HWND, UINT, UINT_PTR, DWORD);
-void OnTick(HWND hWnd);
+VOID CALLBACK OnClock(HWND, UINT, UINT_PTR, DWORD);//function for first Timer's execution
+void OnTick(HWND hWnd);//function for second Timer's execution
 
 void OnCreate(HWND hWnd)//SetTimer.
 {
+	//Timers are made on hWnd(our Window handle.)
 	SetTimer(hWnd, IDT_CLOCK, 1000, OnClock);//IDT_CLOCK(Timer serial number we set), per 1 second, Callback to OnClock
+	//First Timer sends Message per 1 Second.
 	SetTimer(hWnd, IDT_TICK, 1/10, NULL);//IDT_TICK(Timer serial number we set), per 1/100 second, No CallbackProc (***1/10? )
+	//Second Timer sends Message per 1/100 Second.
 }
 
 void OnTimer(HWND hWnd, DWORD tid)
@@ -88,4 +125,24 @@ void OnDestroy(HWND hWnd)
 	PostQuitMessage(0);//WM_QUIT
 }
 
-
+LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)//wParam for get serial number of Timer
+{
+	switch(iMessage)//I Wonder The Way Of Sending Message!!!*****_Timer Message is Sended in Timer object int SetTimer's sending message duration. in first time, call WM_CREATE Message and do work after passing one duration
+	{
+		case WM_CREATE://Create two Timer object.(use execute this program. This MyWndProc is Procedure of window instance we made.)
+			OnCreate(hWnd);
+			return 0;
+			
+		case WM_PAINT://when call InvalidRect in OnClock or OnTick(per Message of Timers).
+			OnPaint(hWnd);
+			return 0;
+		case WM_TIMER://If we send null to Proc in SetTimer, make WM_TIMER message.  (p.s First timer automatcally call that's Proc by TIMERPROC OnClock once.)
+			OnTimer(hWnd, wParam);
+			return 0;
+			
+		case WM_DESTROY://KillTimer all & WM_QUIT(user click close button)
+			OnDestroy(hWnd);
+			return 0;
+	}
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);//recalc, show
+}
